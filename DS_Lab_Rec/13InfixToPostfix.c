3712eeb5268ed528ea2da6608b5c367acd9f040a
Anjana V P <anjanavp231@gmail.com>
2026-05-05 16:45:31 +0530

    Refactor infix to postfix conversion logic

--- DS_Lab_Rec/13InfixToPostfix.c
+++ DS_Lab_Rec/13InfixToPostfix.c
@@ -4,28 +4,33 @@
 char stack[100];
 int top = -1;
 
+/* Push */
 void push(char x)
 {
     stack[++top] = x;
 }
 
+/* Pop */
 char pop()
 {
+    if(top == -1)
+        return -1;
     return stack[top--];
 }
 
+/* Priority */
 int priority(char x)
 {
     if(x == '(')
         return 0;
-
     if(x == '+' || x == '-')
         return 1;
-
     if(x == '*' || x == '/')
         return 2;
+    if(x == '^')
+        return 3;
 
-    return 0;
+    return -1;
 }
 
 int main()
@@ -42,14 +47,19 @@ int main()
 
     while(*e != '\0')
     {
+        /* Operand */
         if(isalnum(*e))
         {
             printf("%c", *e);
         }
+
+        /* Left parenthesis */
         else if(*e == '(')
         {
             push(*e);
         }
+
+        /* Right parenthesis */
         else if(*e == ')')
         {
             while((x = pop()) != '(')
@@ -57,19 +67,23 @@ int main()
                 printf("%c", x);
             }
         }
+
+        /* Operator */
         else
         {
-            while(priority(stack[top]) >= priority(*e))
+            while(top != -1 && 
+                 (priority(stack[top]) > priority(*e) ||
+                 (priority(stack[top]) == priority(*e) && *e != '^')))
             {
                 printf("%c", pop());
             }
-
             push(*e);
         }
 
         e++;
     }
 
+    /* Pop remaining */
     while(top != -1)
     {
         printf("%c", pop());

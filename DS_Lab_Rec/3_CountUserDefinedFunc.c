a0c4df628e4e8ab2fbc2d00a3839c365fa21dba5
Anjana V P <anjanavp231@gmail.com>
2026-07-05 13:49:09 +0530

    Refactor function counting logic and clean up code

--- DS_Lab_Rec/3_CountUserDefinedFunc.c
+++ DS_Lab_Rec/3_CountUserDefinedFunc.c
@@ -1,69 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-int isKeyword(char *word) {
-    char *keywords[] = {
-        "if","for","while","switch","return","printf",
-        "scanf","else","do","case"
-    };
-
-    for(int i = 0; i < 10; i++) {
-        if(strcmp(word, keywords[i]) == 0)
-            return 1;
-    }
-    return 0;
-}
-
-int isDatatype(char *word) {
-    char *types[] = {
-        "int","float","double","char","void","long","short"
-    };
-
-    for(int i = 0; i < 7; i++) {
-        if(strcmp(word, types[i]) == 0)
-            return 1;
-    }
-    return 0;
-}
-
-int main() {
+int main()
+{
     FILE *fp;
-    char filename[100];
-    char line[200];
-    char word1[50], word2[50];
-
+    char filename[50], line[100];
     int count = 0;
-
-    printf("Enter C file name: ");
+    
+    printf("Enter file name: ");
     scanf("%s", filename);
-
     fp = fopen(filename, "r");
-
-    if(fp == NULL) {
+    if (fp == NULL){
         printf("File not found!\n");
         return 0;
     }
 
-    while(fgets(line, sizeof(line), fp)) {
-
-        // Try to extract: datatype function_name(
-        if(sscanf(line, "%s %[^ (](", word1, word2) == 2) {
-
-            // Check: valid datatype + not keyword + not main
-            if(isDatatype(word1) &&
-               !isKeyword(word2) &&
-               strcmp(word2, "main") != 0 &&
-               strchr(line, '{') != NULL) {
-
+    while (fgets(line, sizeof(line), fp))
+    {
+        if (strstr(line, "(") && strstr(line, ")") && strstr(line, "{"))
+        {
+            if (!strstr(line, "main") && !strstr(line, "if") && !strstr(line, "for") &&
+                !strstr(line, "while") && !strstr(line, "switch"))
+            {
                 count++;
             }
         }
     }
-
     fclose(fp);
 
-    printf("Number of user-defined functions: %d\n", count);
-
+    printf("Number of user-defined functions = %d\n", count);
     return 0;
 }

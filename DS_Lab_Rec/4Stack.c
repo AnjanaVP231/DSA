7178ec6bca1af7a3e32ed80063562c63ec37024d
Anjana V P <anjanavp231@gmail.com>
2026-07-05 14:15:25 +0530

    Refactor stack operations for consistency and clarity

--- DS_Lab_Rec/4Stack.c
+++ DS_Lab_Rec/4Stack.c
@@ -8,85 +8,49 @@ int top = -1;
 void push()
 {
     int value;
-
-    if(top == MAX - 1)
-    {
+    if(top == MAX - 1)  {
         printf("Stack Overflow\n");
-    }
-    else
-    {
+    }else {
         printf("Enter value: ");
         scanf("%d", &value);
-
         top++;
         stack[top] = value;
     }
 }
-
-void pop()
-{
-    if(top == -1)
-    {
+void pop(){
+    if(top == -1) {
         printf("Stack Underflow\n");
-    }
-    else
-    {
+    } else {
         printf("Deleted element = %d\n", stack[top]);
         top--;
     }
 }
-
-void display()
-{
+void display(){
     int i;
-
-    if(top == -1)
-    {
+    if(top == -1) {
         printf("Stack Empty\n");
-    }
-    else
-    {
+    } else {
         printf("Stack Elements:\n");
-
-        for(i = top; i >= 0; i--)
-        {
+        for(i = top; i >= 0; i--){
             printf("%d\n", stack[i]);
         }
     }
 }
 
-int main()
-{
+int main(){
     int choice;
-printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
-    do
-    {
-        
+    printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+    do {    
         printf("Enter choice: ");
         scanf("%d", &choice);
-
         switch(choice)
         {
-            case 1:
-                push();
-                break;
-
-            case 2:
-                pop();
-                break;
-
-            case 3:
-                display();
-                break;
-
-            case 4:
-                break;
-
-            default:
-                printf("Invalid Choice\n");
+            case 1:   push();      break;
+            case 2: pop();  break;
+            case 3:  display(); break;
+            case 4:  break;
+            default:  printf("Invalid Choice\n");
         }
-
     } while(choice != 4);
-
     return 0;
 }
